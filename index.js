e9dead2ee13de6478dfe548919518714c549e410
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2021-10-04 01:18:14 +0600

    socket setup done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app=express();
+
+const http=require('http');
+const expressServer=http.createServer(app);
+
+
+
+const {Server}=require('socket.io');
+const io=new Server(expressServer);
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname +'/index.html');
+})
+
+expressServer.listen(3000,()=>{
+    console.log("server listing to port 3000....")
+})
+
