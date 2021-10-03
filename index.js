8917e03bb09442e2e613e5b6a673b3b33454ba72
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2021-10-04 01:53:40 +0600

    connection establishment using socket

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express=require('express');
 const app=express();
-
 const http=require('http');
+
 const expressServer=http.createServer(app);
-
-
 
 const {Server}=require('socket.io');
 const io=new Server(expressServer);
 
+io.on('connection',(socket)=>{
+    console.log( "connection established");
+
+    socket.on('disconnect',()=>{
+        console.log('disconnected')
+    })
+} )
+
+
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname +'/index.html');
+    res.sendFile(__dirname+'/index.html')
 })
 
-expressServer.listen(3000,()=>{
-    console.log("server listing to port 3000....")
-})
-
+expressServer.listen(3000,()=>{console.log("server is running @3000 .....")})
