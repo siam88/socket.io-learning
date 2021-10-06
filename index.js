88c69fd983567b422c89b3f0daa77e7a2658891b
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2021-10-07 00:14:36 +0600

    custom event

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-const express=require('express');
-const app=express();
-const http=require('http');
+const express = require('express');
+const app = new express();
 
-const expressServer=http.createServer(app);
+const http = require('http');
+const expressServer = http.createServer(app);
 
-const {Server}=require('socket.io');
-const io=new Server(expressServer);
+const { Server } = require('socket.io');
+const io = new Server(expressServer);
 
-io.on('connection',(socket)=>{
-    console.log( "connection established");
+io.on('connection', (socket) => {
+    console.log('connected');
+    setTimeout(() => {
+        socket.send(('hello socket learning'))
+    }, 500)
 
-    setTimeout(()=>{
-        socket.send(('hello bangladesh from socket'))
-    },8000)
+    // setInterval(() => {
+    //     let date = new Date();
+    //     let time = date.getTime();
+    //     socket.send(time)
+    // }, 10000)
 
+    setInterval(() => {
+        let date = new Date();
+        let time = date.getTime();
+        socket.emit('customEvent', time)
+    }, 1000)
 
-    socket.on('disconnect',()=>{
-        console.log('disconnected')
+    socket.on('disconnect', () => {
+        console.log("disconnected")
     })
-} )
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname+"/index.html")
 })
 
-expressServer.listen(3000,()=>console.log("server listening 3000"))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+expressServer.listen(3000, () => {
+    console.log('listinging to port 3000')
+})
+
+
+
+
 
 
 
@@ -61,15 +78,41 @@ expressServer.listen(3000,()=>console.log("server listening 3000"))
 // io.on('connection',(socket)=>{
 //     console.log( "connection established");
 
+//     setTimeout(()=>{
+//         socket.send(('hello bangladesh from socket'))
+//     },8000)
+
+//     setInterval(()=>{
+//         let date=new Date();
+//         let time=date.getTime();
+//         socket.send(time)
+//     },500)
+
 //     socket.on('disconnect',()=>{
 //         console.log('disconnected')
 //     })
 // } )
-
-
-
 // app.get('/', (req,res)=>{
-//     res.sendFile(__dirname+'/index.html')
+//     res.sendFile(__dirname+"/index.html")
 // })
 
-// expressServer.listen(3000,()=>{console.log("server is running @3000 .....")})
+// expressServer.listen(3000,()=>console.log("server listening 3000"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
