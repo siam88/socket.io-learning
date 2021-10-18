54f354b6e7cb8280de491423df139a96c83124bc
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2021-10-19 00:31:01 +0600

    working with room

--- socket-combine-chat-app/index.js
+++ socket-combine-chat-app/index.js
@@ -1,27 +1,37 @@
 const express = require('express');
-const app=new express();
+const app = new express();
 const http = require('http');
-const expressServer=http.createServer(app);
+const expressServer = http.createServer(app);
 
 //SOCKET CONNECTION TO SERVER
 const { Server } = require('socket.io');
 const io = new Server(expressServer);
 
-
-io.on('connection', (socket)=>{
-   socket.on('chat', function(msg){
-       io.emit('chat_transfer',msg)
-   })
+//for chatting
+io.on('connection', (socket) => {
+    socket.on('chat', function (msg) {
+        io.emit('chat_transfer', msg)
+    })
 })
 
+
+//for room
+io.on('connection', function (socket) {
+
+    socket.join('kitchen-room');
+    io.sockets.in('kitchen-room').emit('cooking', "COOKING FRIED CHICKEN")
+
+    socket.join('bed-room');
+    io.sockets.in('bed-room').emit('sleeping', "i am sleeping")
+})
 
 
 //VIEW PAGE CALLED
-app.get('/', (req,res)=>{
-     res.sendFile(__dirname+'/index.html')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
 //LISTING PORT
-expressServer.listen(3000, ()=>{
+expressServer.listen(3000, () => {
     console.log("connected to 3000...");
 })
